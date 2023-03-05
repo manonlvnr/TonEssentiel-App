99edac2e422de3b9fcf486e9370a41d2b113c60f
Manon Lavenier <manon.lavenier@gmail.com>
2023-03-05 19:37:27 +0100

    Setup path to the homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Homepage from './components/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className='Homepage'>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+          </Routes>  
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/homepage/Homepage.js
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+        <div className='Homepage'>
+        <h1>Homepage</h1>
+        </div>
+    );
+}
+
+export default Homepage;
