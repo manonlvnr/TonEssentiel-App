e709a3e07a876708c406ea016ba769fc24178298
Manon Lavenier <manon.lavenier@gmail.com>
2023-03-12 18:27:39 +0100

    Display first oils on homepage

--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { IconArrowLeft } from '@tabler/icons-react';
+
+
 function Homepage() {
+    const [oils, setOils] = useState(null);
+
+    useEffect(() => {
+        const fetchOils = async () => {
+            const response = await fetch('/api/oils');
+            const json = await response.json();
+
+            if(response.ok) {
+                setOils(json);
+            }
+        }
+
+        fetchOils();
+    }, []);
+
     return (
         <div className='Homepage'>
         <h1>Homepage</h1>
+        <div>
+            {oils && oils.map(oil => (
+                <div key={oil.id}>
+                    <h2>{oil.name}</h2>
+                    <p>{oil.description}</p>
+                </div>
+            ))}    
+        </div>
+            <IconArrowLeft />
         </div>
     );
 }
