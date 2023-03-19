95d130a525ff08017e4d42e2dde9bb84d06b38ff
Manon Lavenier <manon.lavenier@gmail.com>
2023-03-19 19:19:10 +0100

    Create bottom tab

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import './App.scss';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Homepage from './components/homepage/Homepage';
 import OilsForm from './components/OilsForm';
+import Homepage from './components/pages/Homepage/Homepage';
+import Favorites from './components/pages/Favorites/Favorites';
+import Menu from './components/pages/Menu/Menu';
+import BottomBar from './components/organisms/BottomBar/BottomBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className='Homepage'>
+        <div className=''>
+          <BottomBar />
           <Routes>
             <Route path='/' element={<Homepage />} />
             <Route path='/admin' element={<OilsForm />} />
+            <Route path='/favorites' element={<Favorites />} />
+            <Route path='/menu' element={<Menu />} />
           </Routes>  
         </div>
       </Router>
