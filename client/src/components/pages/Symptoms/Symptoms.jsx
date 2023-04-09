e29765d512d7f2a067c03f443c7eeabf2d176e01
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-09 22:52:14 +0200

    Display the symptoms list

--- client/src/components/pages/Symptoms/Symptoms.jsx
+++ client/src/components/pages/Symptoms/Symptoms.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+
+const symptomsList = ["Aérophargie", "Angine", "Aphte", "Ballonement", "Bronchite", "Constipation", "Courbature", "Diarrhée", "Douleur dentaire", "Engelure", "Grippe", "Hématome", "Hoquet", "Insomnie", "Jambes lourdes", "Mal de dos", "Mal des transports", "mal de tête", "Nausée", "Vomissement", "Poux", "Rhinopharyngite", "Rhumatisme", "Sciatique", "Stress", "Toux", "Verrue", "Virus", "Varice"];
+
 function Symptoms() {
+    const listElement = symptomsList.map(symptom => {
+        return (
+            <Link to={`/symptoms/${symptom}`} className="symptom__container">
+                <li className="symptom__title">{symptom}</li>
+            </Link>
+        )})
+
     return (
-        <h2>Symptoms</h2>
+        <>
+            <h2>Symptoms</h2>
+            <ul className="symptoms__wrapper">
+                {listElement}
+            </ul>
+        </>
     )
 }
 
