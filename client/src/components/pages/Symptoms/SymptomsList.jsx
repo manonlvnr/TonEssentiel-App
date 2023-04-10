1000912b3aefccbe436b226bc7478f5c3fb3e9dd
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-10 20:06:50 +0200

    Rename files

--- client/src/components/pages/Symptoms/SymptomsList.jsx
+++ client/src/components/pages/Symptoms/SymptomsList.jsx
@@ -1,4 +1,4 @@
-import "./Symptoms.scss";
+import "./SymptomsList.scss";
 import AlphabeticalList from "../../molecules/AlphabeticalList/AlphabeticalList";
 
 const symptomsList = ["Aérophargie", "Angine", "Aphte", "Ballonement", "Bronchite", "Constipation", "Courbature", "Diarrhée", "Douleur dentaire", "Engelure", "Grippe", "Hématome", "Hoquet", "Insomnie", "Jambes lourdes", "Mal de dos", "Mal des transports", "Mal de tête", "Nausée", "Vomissement", "Poux", "Rhinopharyngite", "Rhumatisme", "Sciatique", "Stress", "Toux", "Verrue", "Virus", "Varice"];
