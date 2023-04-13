f3c94f6e59a91eca7bed3bdf88afa94b9b926cdb
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-13 18:29:10 +0200

    Get ride of unused request and create request to get oil by name

--- client/src/components/pages/Oil/Oil.jsx
+++ client/src/components/pages/Oil/Oil.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Oil() {
+    const routeParams = useParams();
+
+    const [oil, setOil] = useState([]);
+
+    useEffect(() => {
+        const fetchThemes = async () => {
+        const response = await fetch(`/api/oils/${routeParams.oil}`);
+        const json = await response.json();
+
+            if (response.ok) {
+                setOil(json);
+                console.log(json);
+            }
+        };
+
+        fetchThemes();
+    }, [ routeParams.oil ]);
+
     return (
         <div>
             <h2>Oil</h2>
+            {oil.map((oil) => (
+                <div key={oil.id}>
+                    <h3>{oil.name}</h3>
+                    <p>{oil.description}</p>
+                </div>
+            ))}
         </div>
     );
 }
