15247cb2b2d3ae53f9143de317134d6a200a6d52
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-17 16:04:21 +0200

    Create signout hook and add a button that use it in menu

--- client/src/components/pages/Menu/Menu.jsx
+++ client/src/components/pages/Menu/Menu.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import './Menu.scss';
+import { useSignout } from "../../../hooks/useSignout";
 
 function Menu() {
+    const signout = useSignout()
+
+    const handleclick = () => {
+        signout()
+    }
 
     return (
         <div className="menu__wrapper">
@@ -12,6 +18,7 @@ function Menu() {
                 <li className="menu__link"><Link to="/diffusions">Diffusons</Link></li>
                 <li className="menu__link"><Link to="/themes">Thèmes</Link></li>
             </ul>
+            <button onClick={handleclick}>Déconnexion</button>
         </div>
     )
 }
