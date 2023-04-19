38f03fbbd9a871c65eaefdcefebfbd51fbc1f0be
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-19 15:01:37 +0200

    Create request to get user by id and push new favorites

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const createToken = (_id) => {
     return jwt.sign({ _id }, process.env.SECRET, {
@@ -41,7 +42,44 @@ const signupUsers = async (req, res) => {
     res.json({ message: 'Signup user' });
 };
 
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No user with that id');
+    }
+    try {
+        const user = await User.findById(id);
+        if(user) {
+            return res.status(200).json(user);
+        }
+        res.status(404).json({ message: 'User not found' });
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message, message: 'Error getting user' });
+    }
+}
+
+const updateFavorites = async (req, res) => {
+    const { id } = req.params;
+    const { favorites } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No user with that id');
+    }
+    try {
+        const updateUserFav = await User.findByIdAndUpdate(id, { $push: { favorites: favorites } }, { new: true });
+        res.status(200).json(updateUserFav);
+        return
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+        return
+    }
+}
+
 module.exports = {
     signinUsers,
-    signupUsers
+    signupUsers,
+    getUserById,
+    updateFavorites
 }
