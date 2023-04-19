e3dd91fbfb4be4adea4dfda18ea14e12f05b258f
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-19 15:01:37 +0200

    Create request to get user by id and push new favorites

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { signinUsers, signupUsers } = require('../controllers/usersController');
+const { signinUsers, signupUsers, getUserById, updateFavorites } = require('../controllers/usersController');
 
 // signin
 router.post('/signin', signinUsers);
 
 // Signup
 router.post('/signup', signupUsers);
+
+// Get user by id
+router.get('/:id', getUserById);
+
+// Favorites
+// Mettre les à jour les fav de l'utilisateur
+router.post('/:id', updateFavorites)
 
 module.exports = router
