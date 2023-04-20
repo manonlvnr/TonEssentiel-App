0cc04eb662c381e419ae6030b2e645af785644d9
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-20 11:35:34 +0200

    Add fav logic to the oil page

--- client/src/components/pages/Oil/Oil.jsx
+++ client/src/components/pages/Oil/Oil.jsx
@@ -1,34 +1,103 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useAuthContext } from "../../../hooks/useAuthContext";
 
 function Oil() {
     const routeParams = useParams();
 
     const [oil, setOil] = useState([]);
+    const [userState, setUserState] = useState([]);
+
+    const { user } = useAuthContext();
 
     useEffect(() => {
-        const fetchThemes = async () => {
-        const response = await fetch(`/api/oils/${routeParams.oil}`);
-        const json = await response.json();
-
+        const fetchOil = async () => {
+            const response = await fetch(`/api/oils/${routeParams.oil}`);
+            const json = await response.json();
+            
             if (response.ok) {
                 setOil(json);
-                console.log(json);
+                // console.log(json);
             }
         };
 
-        fetchThemes();
+        fetchOil();
+
     }, [ routeParams.oil ]);
+
+    const handleFavorites = async (e) => {
+        e.preventDefault();
+
+        if(user) {
+            const localInfo = localStorage.getItem('user');
+            const email = JSON.parse(localInfo).email;
+            console.log(email);
+
+            const fetchUser = async () => {
+                const response = await fetch(`/api/users/${email}`);
+                const json = await response.json();
+                
+                if (response.ok) {
+                    setUserState(json);
+                    console.log("userState", json);
+                }
+            }
+            await fetchUser();
+
+            const oilId = await oil.map((oil) => (oil._id))
+            console.log("oil-id" , oilId);
+
+            const favoritesArray = await userState[0].favorites
+            console.log("favorites", favoritesArray);
+
+            if(favoritesArray.includes(oilId[0])) {
+                console.log('oui');
+
+                const removeFavorites = async () => {
+                    const response = await fetch(`/api/users/${email}`, {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({favorites: oilId}),
+                    });
+                    const json = await response.json();
+                    console.log(json);
+                }
+
+                removeFavorites();
+
+            }  else {
+                console.log('non');
+
+                const addFavorites = async () => {
+                    const response = await fetch(`/api/users/${email}`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({favorites: oilId}),
+                    });
+                    const json = await response.json();
+                    console.log(json);
+                }
+
+                addFavorites();
+
+            }
+        }
+    }
 
     return (
         <div>
             <h2>Oil</h2>
             {oil.map((oil) => (
-                <div key={oil.id}>
+                <div key={oil._id}>
                     <h3>{oil.name}</h3>
                     <p>{oil.description}</p>
                 </div>
             ))}
+            <button type="button" onClick={handleFavorites}>favoris</button>
         </div>
     );
 }
