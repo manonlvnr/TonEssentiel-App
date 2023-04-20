19d5113ed067a0501856b67d81139de76b28e20b
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-20 17:27:24 +0200

    Display oils and sort them by alphabetical order

--- client/src/components/pages/AllOils/AllOils.jsx
+++ client/src/components/pages/AllOils/AllOils.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import AlphabeticalList from "../../molecules/AlphabeticalList/AlphabeticalList";
+
+
 function AllOils() {
+    const [oils, setOils] = useState(null);
+    const allOils = []
+
+    useEffect(() => {
+        const fetchOils = async () => {
+            const response = await fetch('/api/oils');
+            const json = await response.json();
+
+            if(response.ok) {
+                setOils(json);
+            }
+        }
+
+        fetchOils();
+
+    }, []);
+
+    oils && oils.map((oil) => {
+        allOils.push(oil.name)
+    })
+
+    console.log(allOils)
+
     return (
-        <h2>AllOils</h2>
+        <div>
+            <h2>AllOils</h2>
+            <div>
+                <AlphabeticalList words={allOils} link="allOils"/>
+            </div>
+        </div>
     )
 }
 
