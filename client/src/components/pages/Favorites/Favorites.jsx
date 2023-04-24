cebc6f7ca90caf5abec3c7a48ce563634ee08b4e
Manon Lavenier <manon.lavenier@gmail.com>
2023-04-24 15:02:19 +0200

    Display favorites oils

--- client/src/components/pages/Favorites/Favorites.jsx
+++ client/src/components/pages/Favorites/Favorites.jsx
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+import Title from '../../atoms/Title/Title'
+
 function Favorites() {
+    const [userState, setUserState] = useState([]);
+
+    useEffect(() => {
+        const fetchFavorites = async () => {
+            const localInfo = localStorage.getItem("user");
+            const email = JSON.parse(localInfo).email;
+
+            const response = await fetch(`/api/users/${email}`);
+            const json = await response.json();
+
+            if (response.ok) {
+                setUserState(json);
+                console.log("userState", json);
+            }
+        };
+
+        fetchFavorites();
+    }, [])
+
     return (
-        <div>Favorites</div>
+        <div className="favorites">
+            <Title children={"Favoris"} />
+            <div className="favorites__container">
+                {userState.map((user) => (
+                    user.favorites.map((favorite) => (
+                        <div className="favorites__container__card">
+                            <div>
+                                <h3>{favorite.name}</h3>
+                                <p>{favorite.description}</p>
+                            </div>
+                        </div>
+                    ))
+                ))}
+            </div>
+        </div>
     )
 }
 
