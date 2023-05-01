60003eeded5b63a46df3957b6daff8f8dbbd3aa9
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-01 23:03:49 +0200

    Wip oil page style

--- client/src/components/pages/Oil/Oil.jsx
+++ client/src/components/pages/Oil/Oil.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuthContext } from "../../../hooks/useAuthContext";
+import parse from "html-react-parser";
+import Title from "../../atoms/Title/Title";
+import "./Oil.scss";
+import { IconPlus, IconMinus, IconHeart, IconHeartFilled } from "@tabler/icons-react";
+import OilSummary from "../../molecules/OilSummary/OilSummary";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 function Oil() {
     const routeParams = useParams();
 
     const [oil, setOil] = useState([]);
     const [userState, setUserState] = useState([]);
+    const [opened, setOpened] = useState([]);
 
     const { user } = useAuthContext();
 
@@ -51,7 +58,9 @@ function Oil() {
         const oilId = await oil.map((oil) => oil._id);
         console.log("oil-id", oilId);
 
-        const favoritesArray = await userState[0].favorites.map(favorite => favorite._id);
+        const favoritesArray = await userState[0].favorites.map(
+            (favorite) => favorite._id
+        );
         console.log("favorites", favoritesArray);
 
         if (favoritesArray.includes(oilId[0])) {
@@ -97,18 +106,113 @@ function Oil() {
         }
     };
 
+    const handleToggle = (index) => {
+        setOpened((prevOpened) => {
+            const newOpened = [...prevOpened];
+            newOpened[index] = !newOpened[index];
+            return newOpened;
+        });
+    };
+    console.log(oil);
     return (
-        <div>
-            <h2>Oil</h2>
-            {oil.map((oil) => (
+        <div className="oil__wrapper">
+            {oil && oil.map((oil) => (
                 <div key={oil._id}>
-                    <h3>{oil.name}</h3>
-                    <p>{oil.description}</p>
+                    <Title children={oil.name} />
+                    <div className="oil__top-info">
+                        <img
+                            src={oil.image}
+                            alt={oil.name}
+                            className="oil__top-info__img"
+                        />
+                        <div className="oil__top-info__description">
+                            <h3 className="oil__top-info__description__title">
+                                Description :
+                            </h3>
+                            {parse(oil.description)}
+                        </div>
+                    </div>
+                    <div className="oil__main-info">
+                        <h3 className="oil__main-info__title">Utile pour :</h3>
+                        {oil && oil.symptoms.map((symptom, index) => (
+                            <div key={symptom.name} className="oil__accordion">
+                                <li
+                                    className={`oil__accordion__item ${
+                                        opened[index] ? "active" : ""
+                                    }`}
+                                >
+                                    <button
+                                        className="item__btn"
+                                        onClick={() => handleToggle(index)}
+                                    >
+                                        <div>{symptom.name}</div>
+                                        <span className="item__control">
+                                            {opened[index] ? (
+                                                <IconMinus />
+                                            ) : (
+                                                <IconPlus />
+                                            )}
+                                        </span>
+                                    </button>
+                                    {symptom && symptom.diffusions.map((diffusion) => (
+                                        <div>
+                                            <div
+                                                key={diffusion.name}
+                                                className={`item__content ${
+                                                    opened[index] ? "open" : ""
+                                                }`}
+                                            >
+                                                <p>{diffusion.name}</p>
+                                                {diffusion.descriptionAlone !==
+                                                null ? (
+                                                    <p>
+                                                        {
+                                                            diffusion.descriptionAlone
+                                                        }
+                                                    </p>
+                                                ) : null}
+                                                {diffusion.descriptionWithOthers !==
+                                                null ? (
+                                                    <p>
+                                                        {
+                                                            diffusion.descriptionWithOthers
+                                                        }
+                                                    </p>
+                                                ) : null}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </li>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="oil__bottom-info">
+                        {oil.OilsAssociated.length ? (
+                            <>
+                                <h3 className="oil__bottom-info__title">Huiles associées :</h3>
+                                <Swiper slidesPerView={"auto"} spaceBetween={8}>
+                                    {oil.OilsAssociated.map((associated) => (
+                                        <SwiperSlide key={associated._id}>
+                                            <Link to={`/allOils/${associated.name}`} key={associated._id}>
+                                                <OilSummary oilInfo={associated} />
+                                            </Link>
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
+                            </>
+                        ) : (
+                            <></>
+                        )}
+                    </div>
                 </div>
             ))}
-            <button type="button" onClick={handleFavorites}>
-                {userState[0]?.favorites?.some(e => e._id === oil[0]._id) ? <p>remove</p> : <p>add</p>}
-            </button>
+            {oil[0] && <button type="button" onClick={handleFavorites} className="oil__favorite-btn">
+                {userState[0]?.favorites?.some((e) => e._id === oil[0]._id) ? (
+                    <IconHeartFilled />
+                ) : (
+                    <IconHeart />
+                )}
+            </button> }
         </div>
     );
 }
