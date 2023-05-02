f7524a49e139eec2f88d2c75576ef6f3974ea7ae
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-02 15:55:22 +0200

    Wip header style

--- client/src/components/organisms/Header/Header.jsx
+++ client/src/components/organisms/Header/Header.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './Header.scss';
+import { IconArrowUpLeft } from '@tabler/icons-react';
 
 function Header() {
+    const navigate = useNavigate();
+	const handleback = () => {
+		navigate(-1, {replace: true});
+	}
+
     return (
         <header className="header">
+            <h1 className='header__name'>Ton Essentiel</h1>
+            <button className='header__btn' onClick={handleback}>
+                <IconArrowUpLeft size={40} />
+            </button>
         </header>
     )
 }
