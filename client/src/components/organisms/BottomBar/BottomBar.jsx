37e4d302550b1efac676ec8049a48235f65200b0
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-07 15:16:00 +0200

    Change bottom bar color

--- client/src/components/organisms/BottomBar/BottomBar.jsx
+++ client/src/components/organisms/BottomBar/BottomBar.jsx
@@ -6,9 +6,9 @@ function BottomBar() {
 
     return (
         <div className="bottom-bar__wrapper">
-            <NavLink to="/" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconHome color="#1D6D1D" size={24} /></NavLink >
-            <NavLink to="/menu" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconListSearch color="#1D6D1D" size={24} /></NavLink >
-            <NavLink to="/favorites" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconHeart color="#1D6D1D" size={24} /></NavLink >
+            <NavLink to="/" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconHome size={24} /></NavLink >
+            <NavLink to="/menu" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconListSearch size={24} /></NavLink >
+            <NavLink to="/favorites" className={({isActive}) => (isActive ? 'bottom-bar__link active' : 'bottom-bar__link')}><IconHeart size={24} /></NavLink >
         </div>
     )
 }
