7f8ccef6998fbd1f9c7ee067fcd35c8f6014247b
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-09 17:17:26 +0200

    Fix symtoms list with dynamic content

--- client/src/components/pages/Symptoms/SymptomsList.jsx
+++ client/src/components/pages/Symptoms/SymptomsList.jsx
@@ -1,22 +1,46 @@
 import "./SymptomsList.scss";
 import AlphabeticalList from "../../molecules/AlphabeticalList/AlphabeticalList";
-import Title from '../../atoms/Title/Title';
+import Title from "../../atoms/Title/Title";
 import Header from "../../organisms/Header/Header";
+import { useEffect, useState } from "react";
 
-const symptoms = ["Aérophargie", "Angine", "Aphte", "Ballonement", "Bronchite", "Constipation", "Courbature", "Diarrhée", "Douleur dentaire", "Engelure", "Grippe", "Hématome", "Hoquet", "Insomnie", "Jambes lourdes", "Mal de dos", "Mal des transports", "Mal de tête", "Nausée", "Vomissement", "Poux", "Rhinopharyngite", "Rhumatisme", "Sciatique", "Stress", "Toux", "Verrue", "Virus", "Varice"];
 
 function SymptomsList() {
+    const [oils, setOils] = useState(null);
+    const symptoms = [];
+
+    useEffect(() => {
+        const fetchOils = async () => {
+            const response = await fetch("/api/oils");
+            const json = await response.json();
+
+            if (response.ok) {
+                setOils(json);
+            }
+        };
+
+        fetchOils();
+    }, []);
+
+    oils && oils.map(oil => {
+        oil.symptoms && oil.symptoms.map(symptom => {
+            symptoms.push(symptom.name)
+        })
+    })
+
+
+    console.log(symptoms);
     return (
         <>
-        <Header />
-        <div className="symptoms__wrapper">
-            <Title children={"Symptômes"} />
-            <div>
-                <AlphabeticalList words={symptoms} link="symptoms"/>
+            <Header />
+            <div className="symptoms__wrapper">
+                <Title children={"Symptômes"} />
+                <div>
+                    <AlphabeticalList words={symptoms} link="symptoms" />
+                </div>
             </div>
-        </div>
         </>
-    )
+    );
 }
 
 export default SymptomsList;
