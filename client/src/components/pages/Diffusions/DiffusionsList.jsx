27dec7dcf2a7eca4d7fe6855636897deb72bfa87
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-10 10:21:42 +0200

    Small fixe

--- client/src/components/pages/Diffusions/DiffusionsList.jsx
+++ client/src/components/pages/Diffusions/DiffusionsList.jsx
@@ -2,7 +2,7 @@ import Title from '../../atoms/Title/Title';
 import Categories from "../../molecules/Categories/Categories";
 import Header from "../../organisms/Header/Header";
 
-const diffusionsCategories = [{name: "Voie orale", path: "/diffusions/voie-orale"}, {name: "Diffusion", path: "/diffusions/diffusion"}, {name: "Massage", path: "/diffusions/massage"}, {name: "Bain", path: "/diffusions/bain"}, {name: "Cosmétique", path: "/diffusions/cosmetique"}, {name: "Inhalation", path: "/diffusions/inhalation"}]
+const diffusionsCategories = [{name: "Voie orale", path: "/diffusions/voie orale"}, {name: "Diffusion", path: "/diffusions/diffusion"}, {name: "Massage", path: "/diffusions/massage"}, {name: "Bain", path: "/diffusions/bain"}, {name: "Cosmétique", path: "/diffusions/cosmétique"}, {name: "Inhalation", path: "/diffusions/inhalation"}]
 
 function DiffusionsList() {
     return (
