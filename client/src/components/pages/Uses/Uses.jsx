666f5ac3553d6ed1d3af7d2948514634f14f9e17
Manon Lavenier <manon.lavenier@gmail.com>
2023-05-19 16:12:36 +0200

    Add content to uses page

--- client/src/components/pages/Uses/Uses.jsx
+++ client/src/components/pages/Uses/Uses.jsx
@@ -1,12 +1,19 @@
 import Title from "../../atoms/Title/Title";
 import Header from "../../organisms/Header/Header";
+import "./Uses.scss";
 
 function Uses() {
     return (
         <>
         <Header />
         <Title children="Précautions d'utilisations" />
-        <div>
+        <div className="uses__wrapper">
+            <div className="uses__content">
+                Les huiles essentielles sont de puissants produits naturels aux propriétés bénéfiques pour la santé.<br/><br/>
+                Cependant, il est important de les utiliser avec précaution. Certaines huiles essentielles peuvent causer des réactions allergiques, irritations ou brûlures si elles sont appliquées directement sur la peau sans dilution.<br/><br/>
+                Il est également important de tenir compte des interactions possibles avec les médicaments et de ne pas ingérer d'huiles essentielles sans l'avis d'un professionnel de la santé.<br/><br/>
+                Il est recommandé de suivre les instructions spécifiques pour chaque huile essentielle et de consulter un professionnel de la santé avant de les utiliser pour des problèmes de santé spécifiques.
+            </div>
         </div>
         </>
     );
